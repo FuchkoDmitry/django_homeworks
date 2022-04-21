e36d49da3d8ee91d1177595f40295298cfe547ae
Dmitry Fuchko <dmitii_fuchko@mail.ru>
2022-04-21 18:37:16 +0300

    2.2 Done

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, ArticleScope
 
-from .models import Article
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tags = 0
+        for form in self.forms:
+            print(form.cleaned_data)
+            if form.cleaned_data.get('is_main', False):
+                main_tags += 1
+        if main_tags != 1:
+            raise ValidationError('Необходимо выбрать один основной раздел ')
+        return super().clean()
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'published_at']
+    list_filter = ('published_at',)
+    inlines = [ArticleScopeInline]
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    # inlines = [ArticleScopeInline]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,53 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    name = models.CharField(
+        max_length=30, verbose_name='Название раздела'
+    )
+    articles = models.ManyToManyField(
+        Article, related_name='tags', through="ArticleScope"
+    )
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.PROTECT,
+        related_name='scopes',
+        verbose_name='Статья'
+    )
+    tag = models.ForeignKey(
+        Scope,
+        on_delete=models.PROTECT,
+        related_name='scopes',
+        verbose_name='Раздел'
+    )
+    is_main = models.BooleanField(verbose_name='Основной раздел', default=False)
+
+    class Meta:
+        verbose_name = 'Раздел статьи'
+        verbose_name_plural = 'Разделы статьи'
+        unique_together = ('article', 'tag')
+        ordering = ['-is_main', 'tag']
+        constraints = (
+            models.UniqueConstraint(
+                fields=('article', 'is_main'),
+                name='one_main_constraint',
+                condition=models.Q(is_main=True)
+            ),
+        )
